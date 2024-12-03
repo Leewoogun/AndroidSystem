36a34ae5b7daf6be177ba4595cfbd7f6ff2a7a43
Leewoogun98 <wglee@logishm.com>
2024-12-03 15:46:31 +0900

    chore: build.gradle 추가

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,38 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import com.lwg.setNamespace
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.lwg.android.library)
+    alias(libs.plugins.lwg.android.hilt)
+    id("kotlinx-serialization")
+}
+
+// properties 파일 로드
+val properties = Properties().apply {
+    load(FileInputStream(rootProject.file("local.properties")))
+}
+fun getApiKey(propertyKey: String): String = gradleLocalProperties(rootDir, providers).getProperty(propertyKey)
+
+android {
+    setNamespace("data")
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        buildConfigField("String", "tmdb_token",getApiKey("tmdb_token"))
+    }
+}
+
+dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.immutable)
+    implementation(libs.kotlinx.datetime)
+}
